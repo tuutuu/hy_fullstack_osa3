fa2bd7d24d8111c5f34a3f53b73e0c2d930e424b
Tuure Piitulainen <tuure.piitulainen@gmail.com>
2019-02-10 21:52:56 +0200

    3.1

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        "persons": [
+            {
+                "name": "Arttu Hella",
+                "number": "050-214555",
+                "id": 1
+            },
+            {
+                "name": "Marto Märvinen",
+                "number": "040-863456",
+                "id": 3
+            },
+            {
+                "name": "Lea Kutvos",
+                "number": "040-123456",
+                "id": 4
+            },
+            {
+                "name": "Tuure Piitulainen",
+                "number": "050-666",
+                "id": 5
+            },
+            {
+                "name": "Mikael Kosola",
+                "number": "050-1244",
+                "id": 6
+            },
+            {
+                "name": "Tuuba Korhonen",
+                "number": "050-7557057",
+                "id": 7
+            }
+        ]
+    }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
