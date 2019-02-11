d9833466fde1d99765a52b2f5ae6d84a0d73c786
Tuure Piitulainen <tuure.piitulainen@gmail.com>
2019-02-11 13:49:16 +0200

    3.3

--- index.js
+++ index.js
@@ -55,6 +55,15 @@ app.get('/info', (req, res) => {
     res.send(info)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons[0].persons.find(person => person.id === id)
+    if (person)
+        res.json(person)
+    else
+        res.status(404).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
